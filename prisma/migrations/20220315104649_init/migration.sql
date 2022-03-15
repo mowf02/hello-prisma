eb4afc666452c1c9b9c168a21d40406bf966c212
mowf02 <hello@lizmowforth.com>
2022-03-15 10:47:31 +0000

    database migration

---
+++ prisma/migrations/20220315104649_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `post` VARCHAR(500) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
